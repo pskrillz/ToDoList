da5c50feb4dcecb9b9fc3abb0838c2ebce3cc854
pi <payam.farah@live.com>
2020-08-02 11:11:40 -0700

    added header

--- ToDoListApp/src/app/material/material.module.ts
+++ ToDoListApp/src/app/material/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
-const Material = [MatButtonModule];
+const Material = [MatButtonModule, MatToolbarModule];
 
 @NgModule({
   imports: [CommonModule, Material],
